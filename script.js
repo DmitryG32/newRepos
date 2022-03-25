a03e9c79895c94738120f388525f9104f1b1ce73
Dmitry Gorbach <89874530087@mail.ru>
2022-03-26 00:19:32 +0100

    add lesson2

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "newProject";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 100;
+let rollback = 20;
+let fullPrice = 2000;
+let adaptive = true;
 
-alert("Hello world");
-console.log("Hello world");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(
+  "Стоимость верстки экранов " +
+    screenPrice +
+    " долларов " +
+    "Стоимость разработки сайта " +
+    fullPrice +
+    " долларов "
+);
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log(
+  "Процент отката посреднику за работу = " + fullPrice * (rollback / 100)
+);
